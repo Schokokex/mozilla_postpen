6c58505326d59a5fa8d7b1782cabe0b0de80a24a
Jochnickel <Jochnickel>
2020-02-13 02:15:24 +0100

    4

--- src/content_script.js
+++ src/content_script.js
@@ -2,4 +2,4 @@ let logger_url = "";
 browser.storage.sync.get("logger_url").then((result)=>{
 	logger_url = result.logger_url;
 	if (""!=logger_url) document.body.style.border = "5px solid red";
-});
+},console.error);
