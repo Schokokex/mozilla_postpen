1388360fa7ce8b51452a0d924e6aa40b78dcbdb9
Jochnickel <Jochnickel>
2020-02-13 01:12:55 +0100

    4

--- src/options.js
+++ src/options.js
@@ -1,12 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-	browser.storage.sync.get("logger_url").then((result)=>{
-		document.querySelector("input").value = result.logger_url || "";
+const CLASS_STORABLE_ITEM = ".store";
+const HTML_ATTR_NAME_STORAGENAME = "storagename";
+
+document.addEventListener('DOMContentLoaded', () => {
+	//how can i start the sync.get before the DOM loads?
+	browser.storage.sync.get().then(store=>{
+		document.querySelectorAll(CLASS_STORABLE_ITEM).forEach(item=>{
+			const itemStorageName = item.attributes[HTML_ATTR_NAME_STORAGENAME].value;
+			item.value = store[itemStorageName] || '';
+		});
 	});
 });
 
-document.querySelector("form").addEventListener("submit", (e)=>{
+document.querySelector('form').addEventListener('submit', e=>{
 	e.preventDefault();
-	browser.storage.sync.set({
-		logger_url: document.querySelector("input").value
+	const toStore = {};
+	document.querySelectorAll(CLASS_STORABLE_ITEM).forEach(item=>{
+		const itemStorageName = item.attributes[HTML_ATTR_NAME_STORAGENAME].value;
+		toStore[itemStorageName] = item.value;
 	});
+	browser.storage.sync.set(toStore);
 });
